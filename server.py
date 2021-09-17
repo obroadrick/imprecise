8af84bc38a5d49d0b2f64d8ecebca21cd1deff47
grahamschock <gschock@gwu.edu>
2021-09-17 11:13:39 -0400

    http server

--- server.py
+++ server.py
@@ -1,29 +1,17 @@
-import socket
-import threading
-import time
-from scheduler import Scheduler
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-def run_scheduler_tasks(scheduler):
-    scheduler.run_tasks()
+port = 8000
+def run_scheduler_tasks(schedule):
+    schedule.run_tasks()
 
-if __name__ == "__main__":
-    s = socket.socket()
-    port = 8888
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
 
-    s.bind(('', port))
+        message = "GET RESPONSE"
+        self.wfile.write(bytes(message, "utf8"))
 
-    s.listen(5)
-
-    schedule = Scheduler()
-
-    t1 = threading.Thread(target=run_scheduler_tasks, args=(schedule, ))
-    t1.start()
-
-
-    while True:
-        c, addr = s.accept()
-        print('Got connection from', addr)
-        task = c.recv(1024).decode()
-        schedule.add_task(task)
-        c.close()
-        break
+with HTTPServer(('', port), handler) as server:
+    server.serve_forever()
