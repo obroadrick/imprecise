16bc79815de444983a89ad7dce7f0a3cd48056bf
grahamschock <gschock@gwu.edu>
2021-10-17 20:02:40 -0400

    added better API for client/server, clients can send req arr and priority

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 scheduler = Scheduler()
 runner = Runner()
 
-def create_new_task(job, deadline):
+def create_new_task(job, deadline, priority):
     stage_files_list = os.listdir("tasks/" + "task" + job)
     stage_files_list.sort()
     stage_list = []
@@ -18,15 +18,16 @@ def create_new_task(job, deadline):
         stage = Stage(stage_file, 0, 0, 0)
         stage_list.append(stage)
 
-    task = Task(stage_list, 0, deadline, "task"+job)
+    task = Task(stage_list, priority, deadline, "task"+job)
     return task
 
-@app.route("/<job>,<deadline>")
-def home(job, deadline):
-    """ Function for test purposes. """
-    task = create_new_task(job, deadline)
-    scheduler.add_task(task)
-    return "Adding job" + str(job) + "deadline" + str(deadline)
+@app.route("/<job_arr>&<deadline>&<priority>")
+def home(job_arr, deadline, priority):
+    jobs = job_arr.split(",")
+    for job in jobs:
+        task = create_new_task(job, deadline, priority)
+        scheduler.add_task(task)
+    return "Adding job" + str(jobs) + "deadline" + str(deadline)
 
 
 if __name__ == "__main__":
