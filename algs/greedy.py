1ed372b874f58d7900eb6715ebb94ace59d64832
Kyle Vitale <62531320+kylevitale2@users.noreply.github.com>
2022-04-27 17:25:58 -0400

    added small notation

--- algs/greedy.py
+++ algs/greedy.py
@@ -74,6 +74,7 @@ class Greedy():
         for taskidx in highest_prio_tasks:
             # see how many layers we can fit in for this task (start at last layer since we have cumulative times)
             for l in range(stages[taskidx]-1, 0, -1):
+                #need to subtract first, since times are cumulative; only want one time for each task, not for them to be added multiple times
                 if time_used + time[taskidx][l] - time[taskidx][0] <= deadline:
                     time_used += time[taskidx][l]
                     time_used -= time[taskidx][0]
